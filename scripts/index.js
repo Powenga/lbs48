5f32a51a8e127694310837e2b10b709976fafb5f
Dmitrii Rakhmanin <rakhmanin.dm@gmail.com>
2021-06-14 14:53:33 +0300

    feat: add photos grid && add img popup

--- scripts/index.js
+++ scripts/index.js
@@ -43,19 +43,61 @@ navNode.addEventListener("click", (evt) => {
   }
 });
 
-//policy popup
+//popup
+const page = document.querySelector(".page")
 const popupPolicy = document.querySelector(".popup_type_policy");
+const popupImage = document.querySelector(".popup_type_img");
+const popupImageImg = popupImage.querySelector(".popup__img");
+const popupImageFigcaption = popupImage.querySelector(".popup__figcaption");
 
-document.querySelectorAll('.policy-btn').forEach(elem => {
+function blockPageScroll() {
+  page.classList.toggle("page_blocked");
+}
+
+function openPopup(popup) {
+  popup.classList.add("popup_active");
+  blockPageScroll();
+}
+
+function setPopupImg(targetImage) {
+  popupImageImg.src = targetImage.src;
+  popupImageImg.alt = targetImage.alt;
+  popupImageFigcaption.textContent = targetImage.alt;
+}
+
+function closePopup() {
+  popupPolicy.classList.remove("popup_active");
+  popupImage.classList.remove("popup_active");
+  blockPageScroll();
+}
+
+document.querySelectorAll(".policy-btn").forEach((elem) => {
   elem.addEventListener("click", (evt) => {
     evt.preventDefault();
-    popupPolicy.classList.add("popup_active");
+    openPopup(popupPolicy);
+  });
+});
+
+document.querySelectorAll(".photos__img").forEach((elem) => {
+  elem.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    setPopupImg(evt.target);
+    openPopup(popupImage);
   });
 });
 
 document.querySelectorAll(".popup__close-btn").forEach((elem) => {
   elem.addEventListener("click", (evt) => {
     evt.preventDefault();
-    popupPolicy.classList.remove("popup_active");
+    closePopup();
   });
 });
+
+document.addEventListener("click", (evt) => {
+  if(evt.target.classList.contains('popup__cover')) {
+    evt.preventDefault();
+    closePopup();
+  }
+});
+
+
